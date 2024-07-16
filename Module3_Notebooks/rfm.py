ab7104fd8226f5ff932a31982a143e67cad34e0f
rkishony <rkishony@technion.ac.il>
2024-07-15 20:42:59 -0400

    streamline rfm

--- Module3_Notebooks/rfm.py
+++ Module3_Notebooks/rfm.py
@@ -4,57 +4,47 @@ from sklearn.base import BaseEstimator
 import time
 from tqdm import tqdm
 
-def euclidean_distances(samples, centers, M=None, squared=True):
-    if M is None:
-        samples_norm = np.sum(samples**2, axis=1, keepdims=True)
-    else:
-        samples_norm = (samples @ M)  * samples
-        samples_norm = np.sum(samples_norm, axis=1, keepdims=True)
-    if samples is centers:
-        centers_norm = samples_norm
-    else:
-        if M is None:
-            centers_norm = np.sum(centers**2, axis=1, keepdims=True)
-        else:
-            centers_norm = (centers @ M) * centers
-            centers_norm = np.sum(centers_norm, axis=1, keepdims=True)
 
-    centers_norm = np.reshape(centers_norm, (1, -1))
+def get_norm(x, M=None):
+    x2 = x * x if M is None else (x @ M) * x
+    return np.sum(x2, axis=1, keepdims=True)
+
+
+def euclidean_distances(samples, centers, M=None, squared=True, threshold=None):
+    samples_norm = get_norm(samples, M)
+    centers_norm = get_norm(centers, M) if samples is not centers else samples_norm
     distances = samples @ (M @ centers.T)
-    distances *= -2
-    distances = distances + samples_norm + centers_norm
+    distances = -2 * distances + samples_norm + centers_norm.T
+    if threshold is not None:
+        distances[distances < threshold] = 0
     if not squared:
-        distances = np.where(distances < 0, 0, distances)
         distances = np.sqrt(distances)
 
     return distances
 
+
 def laplace_kernel(samples, centers, bandwidth, M=None):
     assert bandwidth > 0
-    kernel_mat = euclidean_distances(samples, centers, M=M, squared=False)
-    kernel_mat = np.where(kernel_mat < 0, 0, kernel_mat)
-    gamma = 1. / bandwidth
-    kernel_mat *= -gamma
-    kernel_mat = np.exp(kernel_mat)
+    kernel_mat = euclidean_distances(samples, centers, M=M, squared=False, threshold=0)
+    kernel_mat = np.exp(-kernel_mat / bandwidth)
     return kernel_mat
 
 
 def get_grads(X, sol, L, P, max_num_samples=20000, centering=True):
-    indices = np.random.randint(len(X), size=max_num_samples)
-
-    if len(X) > len(indices):
+    if len(X) > max_num_samples:
+        indices = np.random.randint(len(X), size=max_num_samples)
         x = X[indices, :]
     else:
         x = X
 
     K = laplace_kernel(X, x, L, M=P)
 
-    dist = euclidean_distances(X, x, M=P, squared=False)
-    dist = np.where(dist < 1e-10, 0, dist)
+    dist = euclidean_distances(X, x, M=P, squared=False, threshold=1e-10)
 
     with np.errstate(divide='ignore'):
-        K = K/dist
+        K = K / dist
 
+    # RK: Why set to 0 and not some large number?
     K[K == float("Inf")] = 0.
 
     a1 = sol.T
@@ -86,98 +76,85 @@ def get_grads(X, sol, L, P, max_num_samples=20000, centering=True):
 
     G = (step2 - step3) * -1/L
     if centering:
-        G_mean = np.expand_dims(np.mean(G, axis=0), axis=0)
-        G = G - G_mean
+        G -= np.mean(G, axis=0, keepdims=True)
     return G
 
 def egop(G, verbose=False, diag_only=False):
     M = 0.
     chunks = len(G) // 20 + 1
     batches = np.array_split(G, chunks)
+    i_range = range(len(batches))
     if verbose:
-        for i in tqdm(range(len(batches))):
-            grad = batches[i]
-            gradT = np.swapaxes(grad, 1, 2)
-            if diag_only:
-                T = np.sum(gradT * gradT, axis=-1)
-                M += np.sum(T, axis=0)
-            else:
-                M += np.sum(gradT @ grad, axis=0)
-                del grad, gradT
-    else:
-        for i in range(len(batches)):
-            grad = batches[i]
-            gradT = np.swapaxes(grad, 1, 2)
-            if diag_only:
-                T = np.sum(gradT * gradT, axis=-1)
-                M += np.sum(T, axis=0)
-            else:
-                M += np.sum(gradT @ grad, axis=0)
-                del grad, gradT
+        i_range = tqdm(i_range)
+    for i in i_range:
+        grad = batches[i]
+        gradT = np.swapaxes(grad, 1, 2)
+        if diag_only:
+            T = np.sum(gradT * gradT, axis=-1)
+        else:
+            T = gradT @ grad
+        M += np.sum(T, axis=0)
+        del grad, gradT
     M /= len(G)
     if diag_only:
         M = np.diag(M)
     return M
 
+
 class RFM(BaseEstimator):
     def __init__(self, kernel="laplace"):
-        self.kernel=kernel
+        self.kernel = kernel
         self.X_train = None
         self.alphas = None
         self.M = None
         self.L = None
         self.reg = None
+        self.verbose = False
+
+    def print_if_verbose(self, message):
+        if self.verbose:
+            print(message)
 
     def fit(self, X_train, y_train, reg=1e-3, bandwidth=10, num_iters=5,
             M=None, centering=True, verbose=False, diag_only=False):
         self.X_train = X_train
+        self.verbose = verbose
         n, d = X_train.shape
         if M is None:
             M = np.eye(d)
         self.M = M
         self.L = bandwidth
         self.reg = reg
-
-        for iter_idx in range(num_iters):
-            if verbose:
-                print("Starting Iteration: " + str(iter_idx))
+        iter_idx = -1
+        while True:
+            iter_idx += 1
+            self.print_if_verbose(f"Starting Iteration: {iter_idx}")
             start = time.time()
             K_train = laplace_kernel(X_train, X_train, self.L, M=M)
             sol = solve(K_train + reg * np.eye(n), y_train).T
             end = time.time()
-            if verbose:
-                print("Solved Kernel Regression in " + str(end - start) + " seconds.")
+            self.print_if_verbose(f"Solved Kernel Regression in {end - start} seconds.")
             self.alphas = sol
+            if iter_idx == num_iters:
+                break
 
             start = time.time()
             G = get_grads(X_train, self.alphas, self.L, M, centering=centering)
             end = time.time()
-            if verbose:
-                print("Computed Gradients in "  + str(end - start) + " seconds.")
+            self.print_if_verbose(f"Computed Gradients in {end - start} seconds.")
 
             start = time.time()
             M = egop(G, verbose=verbose, diag_only=diag_only)
             end = time.time()
-            if verbose:
-                print("Computed EGOP in " + str(end - start) + " seconds.")
-                print("===============================================================")
+            self.print_if_verbose(f"Computed EGOP in {end - start}\n{'='*60}")
             self.M = M
 
-        start = time.time()
-        K_train = laplace_kernel(X_train, X_train, self.L, M=M)
-        sol = solve(K_train + reg * np.eye(n), y_train).T
-        end = time.time()
-        if verbose:
-            print("Solved Final Kernel Regression in " + str(end - start) + " seconds.")
         self.alphas = sol
         return self
 
     def predict(self, X_test):
-        L = self.L
-        M = self.M
-        K_test = laplace_kernel(self.X_train, X_test, L, M=M)
-        preds = (self.alphas @ K_test).T
-        return preds
+        K_test = laplace_kernel(self.X_train, X_test, self.L, M=self.M)
+        return (self.alphas @ K_test).T
 
     def get_alphas(self):
         return self.alphas
---
+++ Module3_Notebooks/test_rfm.py
@@ -0,0 +1,19 @@
+import numpy as np
+import rfm
+
+
+def test_rfm():
+    x_train = np.array([[1., 0], [2, 1], [3, 0], [4, 1], [5, 0], [6, 1]])
+    y_train = x_train[:, 0]
+    y_train = y_train.reshape(-1, 1)
+
+    model = rfm.RFM()
+    model = model.fit(x_train, y_train, num_iters=2, reg=1e-4,
+                      centering=True, verbose=False, diag_only=False)
+
+    M = model.get_M()
+    expectedc_M = np.array([
+        [0.07748122, 0.01431913],
+        [0.01431913, 0.0038696]]
+    )
+    assert np.allclose(M, expectedc_M)
